2a69403e48b7af0eaf6871ab835ddf4d04a9a69a
Shunya IREIJO <e205760@ie.u-ryukyu.ac.jp>
2021-12-09 00:14:14 +0900

    ステップ3でのカプセル化

--- app/src/main/java/Main.java
+++ app/src/main/java/Main.java
@@ -4,11 +4,15 @@ public class Main {
     public static void main(String[] args){
         Hero hero = new Hero("勇者", 10, 5);
         Enemy enemy = new Enemy("スライム", 6, 3);
+        String hero_name = hero.getName();
+        String enemy_name = enemy.getName();
+        boolean hero_dead = hero.getDead();
+        boolean enemy_dead = enemy.getDead();
 
-        System.out.printf("%s vs. %s\n", hero.name, enemy.name);
+        System.out.printf("%s vs. %s\n", hero_name, enemy_name);
 
         int turn = 0;
-        while( hero.dead == false && enemy.dead == false ){
+        while( hero_dead == false && enemy_dead == false ){
             turn++;
             System.out.printf("%dターン目開始！\n", turn);
             hero.attack(enemy);
--- app/src/test/java/jp/ac/uryukyu/ie/e205760/EnemyTest.java
+++ app/src/test/java/jp/ac/uryukyu/ie/e205760/EnemyTest.java
@@ -17,8 +17,9 @@ class EnemyTest {
         int defaultHeroHp = 100;
         Hero demoHero = new Hero("デモ勇者", defaultHeroHp, 100);
         Enemy slime = new Enemy("スライムもどき", 10, 100);
+        int demoHero_hitPoint = demoHero.getHitPoint();
         demoHero.attack(slime);
         slime.attack(demoHero);
-        assertEquals(defaultHeroHp, demoHero.hitPoint);
+        assertEquals(defaultHeroHp, demoHero_hitPoint);
     }
 }
